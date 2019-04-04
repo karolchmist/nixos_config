47146c392be0d7e8f390ecdca38854a8a4d420ef
Karol Chmist <karol@chmist.com>
2019-04-04 12:05:58 +0200

    Fix formatting

--- configuration.nix
+++ configuration.nix
@@ -5,140 +5,140 @@
 { config, pkgs, ... }:
 
 {
-  imports =
+    imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./ssd-configuration.nix
-      ./xserver-configuration.nix
-      ./lenovo-t460p-configuration.nix
-      ./wpa.nix
+        ./hardware-configuration.nix
+        ./ssd-configuration.nix
+        ./xserver-configuration.nix
+        ./lenovo-t460p-configuration.nix
+        ./wpa.nix
     ];
 
-  # Use the gummiboot efi boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # Use the gummiboot efi boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  # See https://github.com/anderspapitto/nixos-configuration
-  nix.nixPath = [
-		"/etc/nixos"
-		"nixos-config=/etc/nixos/configuration/configuration.nix"
-	];
+    # See https://github.com/anderspapitto/nixos-configuration
+    nix.nixPath = [
+        "/etc/nixos"
+        "nixos-config=/etc/nixos/configuration/configuration.nix"
+    ];
 
-  networking = {
-	extraHosts = "
-		127.0.0.1   localhost spark-master hive-metastore hive-server namenode kafka datanode
-		::1         localhost spark-master hive-metastore hive-server namenode kafka datanode
-	";
-	hostName = "hermes"; # Define your hostname.
-  	wireless = {
-		enable = true;  # Enables wireless support via wpa_supplicant.
-		userControlled.enable = true;
-	};
-	firewall = {
-		enable = true;
-		allowedTCPPorts = [];
-		allowedUDPPorts = [];
-	};
-  };
+    networking = {
+        extraHosts = "
+            127.0.0.1   localhost spark-master hive-metastore hive-server namenode kafka datanode
+            ::1         localhost spark-master hive-metastore hive-server namenode kafka datanode
+        ";
+        hostName = "hermes"; # Define your hostname.
+        wireless = {
+            enable = true;  # Enables wireless support via wpa_supplicant.
+            userControlled.enable = true;
+        };
+        firewall = {
+            enable = true;
+            allowedTCPPorts = [];
+            allowedUDPPorts = [];
+        };
+    };
 
-   i18n = {
-    consoleKeyMap = "pl";
-    defaultLocale = "pl_PL.UTF-8";
+    i18n = {
+        consoleKeyMap = "pl";
+        defaultLocale = "pl_PL.UTF-8";
    };
 
-  fonts.enableGhostscriptFonts = true;
+    fonts.enableGhostscriptFonts = true;
 
-  hardware = {
-    pulseaudio.enable = true;
-    # steam
-    opengl.driSupport32Bit = true;
-    opengl.driSupport = true;
-    pulseaudio.support32Bit = true;
+    hardware = {
+        pulseaudio.enable = true;
+        # steam
+        opengl.driSupport32Bit = true;
+        opengl.driSupport = true;
+        pulseaudio.support32Bit = true;
 
-    sane.enable = true; # scanning
-  };
+        sane.enable = true; # scanning
+    };
 
-  time.timeZone = "Europe/Paris";
+    time.timeZone = "Europe/Paris";
 
-  nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    wget
-    htop
-    iotop
-    vim
-    p7zip
-    zip
-    traceroute
-    iftop
-    fuse_exfat
-    sakura # default terminal
-    keepassx
-    pmount
+    environment.systemPackages = with pkgs; [
+        wget
+        htop
+        iotop
+        vim
+        p7zip
+        zip
+        traceroute
+        iftop
+        fuse_exfat
+        sakura # default terminal
+        keepassx
+        pmount
 
-    hplip
-  ];
+        hplip
+    ];
 
 
-  programs.zsh.enable = true;
+    programs.zsh.enable = true;
 
-  services = {
-	dictd = {
-		enable = true;
-		DBs = with pkgs; [
-			dictdDBs.eng2fra
-			dictdDBs.fra2eng
-		 ];
-	};
+    services = {
+        dictd = {
+            enable = true;
+            DBs = with pkgs; [
+                dictdDBs.eng2fra
+                dictdDBs.fra2eng
+            ];
+        };
 
-	gnome3.gnome-keyring.enable = true;
+        gnome3.gnome-keyring.enable = true;
 
-	nixosManual.showManual = true;
+        nixosManual.showManual = true;
 
-	printing = {
-		enable = true;
-		drivers = [ pkgs.hplipWithPlugin ];
-	};
-	saned.enable = true;
+        printing = {
+            enable = true;
+            drivers = [ pkgs.hplipWithPlugin ];
+        };
 
-	redshift = {
-	  enable = true;
-	  latitude = "45.76";
-	  longitude = "4.84";
-	  brightness = {
-		 day = "1";
-		 night = "0.8";
-	  };
-	};
+        saned.enable = true;
 
-	udev.extraRules = ''
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666", GROUP="plugdev"
-		SUBSYSTEM=="usb", ATTR{idVendor}=="22b8", MODE="0666", GROUP="plugdev"
-		SUBSYSTEM=="usb", ATTR{idVendor}=="0fce", MODE="0666", GROUP="plugdev"
-		SUBSYSTEM=="usb", ATTR{idVendor}=="045e", MODE="0666", GROUP="plugdev"
-		SUBSYSTEM=="usb", ATTR{idVendor}=="2ae5", MODE="0666", GROUP="plugdev"
-	'';
-  };
+        redshift = {
+            enable = true;
+            latitude = "45.76";
+            longitude = "4.84";
+            brightness = {
+                day = "1";
+                night = "0.8";
+            };
+        };
+
+        udev.extraRules = ''
+            SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666", GROUP="plugdev"
+            SUBSYSTEM=="usb", ATTR{idVendor}=="22b8", MODE="0666", GROUP="plugdev"
+            SUBSYSTEM=="usb", ATTR{idVendor}=="0fce", MODE="0666", GROUP="plugdev"
+            SUBSYSTEM=="usb", ATTR{idVendor}=="045e", MODE="0666", GROUP="plugdev"
+            SUBSYSTEM=="usb", ATTR{idVendor}=="2ae5", MODE="0666", GROUP="plugdev"
+        '';
+    };
   
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.karol = {
-    isNormalUser = true;
-    home = "/home/karol";
-    extraGroups = ["wheel" "networkmanager" "docker" "plugdev" "scanner"];
-    uid = 1001;
-    shell = "/run/current-system/sw/bin/zsh";
-  };
-  users.extraUsers.guest = {
-    isNormalUser = true;
-    home = "/home/guest";
-    extraGroups = [];
-    uid = 1002;
-    shell = "/run/current-system/sw/bin/zsh";
-  };
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.extraUsers.karol = {
+        isNormalUser = true;
+        home = "/home/karol";
+        extraGroups = ["wheel" "networkmanager" "docker" "plugdev" "scanner"];
+        uid = 1001;
+        shell = "/run/current-system/sw/bin/zsh";
+    };
+    users.extraUsers.guest = {
+        isNormalUser = true;
+        home = "/home/guest";
+        extraGroups = [];
+        uid = 1002;
+        shell = "/run/current-system/sw/bin/zsh";
+    };
 
+    security.sudo.wheelNeedsPassword = false;
 
-  security.sudo.wheelNeedsPassword = false;
-
-  virtualisation.docker.enable = true;
-  virtualisation.docker.enableOnBoot = false;
+    virtualisation.docker.enable = true;
+    virtualisation.docker.enableOnBoot = false;
 }
