fd01fb83ce60f459ba22301d63f102967b644a95
Karol Chmist <karol@chmist.com>
2019-04-17 22:12:18 +0200

    Format xserver-configuration.nix

--- xserver-configuration.nix
+++ xserver-configuration.nix
@@ -1,38 +1,38 @@
 { config, lib, pkgs, ... }:
 
 {
-	services.xserver = {
-		enable = true;
-		xkbOptions = "compose:caps, eurosign:5";
-		#xserverArgs = [ "-dpi 144" ];
+  services.xserver = {
+    enable = true;
+    xkbOptions = "compose:caps, eurosign:5";
+    #xserverArgs = [ "-dpi 144" ];
 
-		displayManager.lightdm.enable = true;
+    displayManager.lightdm.enable = true;
 
-		desktopManager.xfce = {
-			enable = true;
-			noDesktop = true;
-			enableXfwm = false;
-		};
-		desktopManager.default = "xfce";
-		
-		### i3
-		windowManager.i3 = {
-			enable = true;
-			extraPackages = with pkgs; [
-				dmenu
-				i3lock
-			];
-		};
-		windowManager.default = "i3";
-	};
-	
-	environment.systemPackages = with pkgs; [
-		nitrogen # wallpaper setter
-		slock # X display locker
-		(polybar.override {
-			i3Support = true;	
-			mpdSupport = true;
-		})
-	];
+    desktopManager.xfce = {
+      enable = true;
+      noDesktop = true;
+      enableXfwm = false;
+    };
+    desktopManager.default = "xfce";
+    
+    ### i3
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3lock
+      ];
+    };
+    windowManager.default = "i3";
+  };
+  
+  environment.systemPackages = with pkgs; [
+    nitrogen # wallpaper setter
+    slock # X display locker
+    (polybar.override {
+      i3Support = true;  
+      mpdSupport = true;
+    })
+  ];
 
 }
