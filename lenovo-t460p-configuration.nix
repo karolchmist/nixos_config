c71467acd167c1e03078c78934548a3d976d8dd0
Karol Chmist <karol@chmist.com>
2019-08-28 14:25:22 +0200

    Add tlp to lenovo-t460p-configuration.nix

--- lenovo-t460p-configuration.nix
+++ lenovo-t460p-configuration.nix
@@ -1,15 +1,27 @@
 { config, lib, pkgs, ... }:
 
 {
-	hardware = {
-		bluetooth.enable = false;
-	};
+    hardware = {
+        bluetooth.enable = false;
+    };
 
-	services.xserver = {
-		xkbModel = "pc105";
+    services.xserver = {
+        xkbModel = "pc105";
 
-		libinput = {
-			enable = true;
-		};
-	};
+        libinput = {
+            enable = true;
+        };
+        
+        tlp = {
+            enable = true;
+            extraConfig = ''
+                 START_CHARGE_THRESH_BAT0=45
+                 STOP_CHARGE_THRESH_BAT0=60 
+                 WIFI_PWR_ON_AC=off
+                 WIFI_PWR_ON_BAT=off
+                 DEVICES_TO_DISABLE_ON_LAN_CONNECT="wifi wwan"
+            '';
+        };
+    };
+
 }
