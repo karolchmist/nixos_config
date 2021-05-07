ce7fdf5516195b5c2e2cb71d929c0eb964de927e
Karol Chmist <info+github@chmist.com>
2021-05-07 18:06:18 +0200

    Fix tlp deprecation : extraConfig -> settings

--- lenovo-t460p-configuration.nix
+++ lenovo-t460p-configuration.nix
@@ -10,13 +10,13 @@
     services = {
         tlp = {
             enable = true;
-            extraConfig = ''
-                 START_CHARGE_THRESH_BAT0=45
-                 STOP_CHARGE_THRESH_BAT0=60 
-                 WIFI_PWR_ON_AC=off
-                 WIFI_PWR_ON_BAT=off
-                 DEVICES_TO_DISABLE_ON_LAN_CONNECT="wifi wwan"
-            '';
+            settings = {
+                "START_CHARGE_THRESH_BAT0" = 45;
+                "STOP_CHARGE_THRESH_BAT0" = 60;
+                "WIFI_PWR_ON_AC" = "off";
+                "WIFI_PWR_ON_BAT" = "off";
+                "DEVICES_TO_DISABLE_ON_LAN_CONNECT" = "wifi wwan";
+            };
         };
         
         xserver = {
